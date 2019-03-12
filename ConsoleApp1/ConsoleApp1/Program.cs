a5409ee03f5078a0a97452aec831cbad8755e2b3
lv <jkhbjk>
2019-03-12 12:43:17 +0700

    sc

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("3. A + B");
+            int sum = a + b;
+            Console.WriteLine("A + B = {0}", sum);
+        }
+    }
+}
